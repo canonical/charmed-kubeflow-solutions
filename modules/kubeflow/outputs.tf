28029b0289e780b17f2bb209d0b95ea9fc52369b
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-10-11 12:25:48 +0300

    reformat outputs.tf + add kserve_controller output

--- modules/kubeflow/outputs.tf
+++ modules/kubeflow/outputs.tf
@@ -1,14 +1,22 @@
 output "grafana_agent_k8s" {
   value = var.cos_configuration ? {
-    app_name : var.existing_grafana_agent_name == null ? one(juju_application.grafana_agent_k8s[*].name) : var.existing_grafana_agent_name
-    provides : {
-      grafana_dashboards_provider : "grafana-dashboards-provider",
+    app_name = var.existing_grafana_agent_name == null ? one(juju_application.grafana_agent_k8s[*].name) : var.existing_grafana_agent_name
+    provides = {
+      grafana_dashboards_provider = "grafana-dashboards-provider",
     }
-    requires : {
-      logging_consumer : "logging-consumer",
-      send_remote_write : "send-remote-write",
+    requires = {
+      logging_consumer  = "logging-consumer",
+      send_remote_write = "send-remote-write",
     }
   } : null
+}
+
+output "kserve_controller" {
+  value = {
+    app_name = module.kserve_controller.app_name,
+    provides = module.kserve_controller.provides,
+    requires = module.kserve_controller.requires,
+  }
 }
 
 output "model_name" {
@@ -17,7 +25,7 @@ output "model_name" {
 
 output "tls_certificate_requirer" {
   value = {
-    app_name : module.istio_pilot.app_name,
-    requires : module.istio_pilot.requires.certificates
+    app_name = module.istio_pilot.app_name,
+    requires = module.istio_pilot.requires.certificates
   }
 }
