065219be9a801d885a3041292fe5776c21b479f4
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-10-15 11:45:45 +0300

    update outputs

--- modules/kubeflow-mlflow/outputs.tf
+++ modules/kubeflow-mlflow/outputs.tf
@@ -5,3 +5,10 @@ output "grafana_agent_k8s" {
 output "model" {
   value = module.kubeflow.model
 }
+
+output "tls_certificate_requirer" {
+  value = {
+    app_name = module.kubeflow.tls_certificate_requirer.app_name,
+    requires = module.kubeflow.tls_certificate_requirer.requires,
+  }
+}
