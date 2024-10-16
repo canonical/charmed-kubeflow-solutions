b7e5b80766bec63618547c8457992497d4307418
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-10-16 13:10:44 +0300

    outputs: Add resource_dispatcher

--- modules/kubeflow-mlflow/outputs.tf
+++ modules/kubeflow-mlflow/outputs.tf
@@ -6,6 +6,14 @@ output "model" {
   value = module.kubeflow.model
 }
 
+output "resource_dispatcher" {
+  value = {
+    app_name = module.resource_dispatcher.app_name,
+    provides = module.resource_dispatcher.provides,
+    requires = module.resource_dispatcher.requires,
+    }
+}
+
 output "tls_certificate_requirer" {
   value = {
     app_name = module.kubeflow.tls_certificate_requirer.app_name,
