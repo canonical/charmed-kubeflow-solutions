0a2da2156dd046cf40a8eb080e1b889986d77c7a
Tevfik Oguzhan Aksoy <tevfik.aksoy@canonical.com>
2025-10-07 10:37:11 +0300

    fix: change source reference of mlflow_mysql from main to commit

--- modules/kubeflow/applications.tf
+++ modules/kubeflow/applications.tf
@@ -83,10 +83,10 @@ module "katib_controller" {
 
 module "katib_db" {
   # tflint-ignore: terraform_module_pinned_source
-  source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
+  source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=93d6608632ae791bffce18e3b74816ebe26d1c5a"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
-  app_name        = "katib-db"
-  channel         = "8.0/stable"
+  app_name   = "katib-db"
+  channel    = "8.0/stable"
   # The following config is equivalent to "constraints: mem=2G"
   config = {
     profile-limit-memory = "2048"
@@ -121,10 +121,10 @@ module "kfp_api" {
 
 module "kfp_db" {
   # tflint-ignore: terraform_module_pinned_source
-  source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
+  source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=93d6608632ae791bffce18e3b74816ebe26d1c5a"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
-  app_name        = "kfp-db"
-  channel         = "8.0/stable"
+  app_name   = "kfp-db"
+  channel    = "8.0/stable"
   # The following config is equivalent to "constraints: mem=2G"
   config = {
     profile-limit-memory = "2048"
