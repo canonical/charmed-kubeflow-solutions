567c56fbf3648786dcebcc57bfd6a53234814489
Dario Faccin <dario.faccin@canonical.com>
2025-10-29 18:51:18 +0100

    chore: hardcode edge for Kubeflow Trainer charm
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- modules/kubeflow/applications.tf
+++ modules/kubeflow/applications.tf
@@ -256,7 +256,7 @@ module "kubeflow_trainer" {
   source     = "git::https://github.com/canonical/training-operator//terraform?ref=track/2.0"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
   revision   = var.kubeflow_trainer_revision
-  channel    = "2.0/${var.risk}"
+  channel    = "2.0/edge"
 }
 
 module "kubeflow_volumes" {
