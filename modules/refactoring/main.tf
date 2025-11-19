0035598aa1c20050fa095f5f0c581f181e9ef6d5
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-11-19 19:02:26 +0100

    multi-model offer/endpoint test

--- modules/refactoring/main.tf
+++ modules/refactoring/main.tf
@@ -8,13 +8,43 @@ resource "juju_model" "kubeflow" {
   }
 }
 
+module "core" {
+  depends_on = [juju_model.kubeflow]
+  source = "./modules/core"
+  model = juju_model.kubeflow.uuid
+}
+
+resource "juju_offer" "ingress" {
+  application_name = module.core.offers.ingress.name
+  endpoints = [module.core.offers.ingress.endpoint]
+  model_uuid       = juju_model.kubeflow.uuid
+}
+
+resource "juju_offer" "dashboard_links" {
+  application_name = module.core.offers.dashboard_links.name
+  endpoints = [module.core.offers.dashboard_links.endpoint]
+  model_uuid       = juju_model.kubeflow.uuid
+}
+
+
+resource "juju_model" "katib" {
+  name  = "katib"
+  # owner = "admin"
+  config = {
+    juju-http-proxy = var.http_proxy
+    juju-https-proxy = var.https_proxy
+    juju-no-proxy = var.no_proxy
+  }
+}
+
+
 # Single MySQL database
 module "db" {
   depends_on = [juju_model.kubeflow]
   count        = var.db.deployed == "shared" ? 1 : 0
   # tflint-ignore: terraform_module_pinned_source
   source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=58072079edc97bace08b6ff9c8f380b94867ebd4"
-  model = juju_model.kubeflow.uuid
+  model = juju_model.katib.uuid
   app_name   = "mysql"
   channel    = "8.0/stable"
   # The following config is equivalent to "constraints: mem=2G"
@@ -25,19 +55,20 @@ module "db" {
   revision     = var.db.info.revision
 }
 
-module "core" {
-  depends_on = [juju_model.kubeflow, module.db]
-  source = "./modules/core"
-  model = juju_model.kubeflow.uuid
-}
 
 module "katib" {
-  depends_on = [module.core, module.db, juju_model.kubeflow]
+  depends_on = [module.core, module.db, juju_model.katib]
   count = contains(var.components, "katib") ? 1 : 0
   source = "./modules/katib"
-  model = juju_model.kubeflow.uuid
-  ingress = module.core.offers.ingress
-  dashboard_links = module.core.offers.dashboard_links
+  model = juju_model.katib.uuid
+  ingress = {
+    kind = "offer",
+    url = juju_offer.ingress.url
+  }
+  dashboard_links = {
+    kind = "offer"
+    url = juju_offer.dashboard_links.url
+  }
 
   # Dedicated DB
   db = var.db.deployed == "private" ? {
--- modules/refactoring/modules/katib/integrations.tf
+++ modules/refactoring/modules/katib/integrations.tf
@@ -1,9 +1,24 @@
 resource "juju_integration" "istio_pilot_katib_uiingress" {
+  count = var.ingress.kind == "endpoint" ? 1 : 0
   model_uuid = data.juju_model.kubeflow.uuid
 
   application {
     name     = var.ingress.name
     endpoint = var.ingress.endpoint
+  }
+
+  application {
+    name     = juju_application.katib_ui.name
+    endpoint = "ingress"
+  }
+}
+
+resource "juju_integration" "istio_pilot_katib_uiingress_offer" {
+  count = var.ingress.kind == "offer" ? 1 : 0
+  model_uuid = data.juju_model.kubeflow.uuid
+
+  application {
+    offer_url = var.ingress.url
   }
 
   application {
@@ -41,11 +56,26 @@ resource "juju_integration" "katib_db_manager_katib_db_relational_db" {
 }
 
 resource "juju_integration" "kubeflow_dashboard_katib_ui_links" {
+  count = var.dashboard_links.kind == "endpoint" ? 1 : 0
   model_uuid = data.juju_model.kubeflow.uuid
 
   application {
     name     = var.dashboard_links.name
     endpoint = var.dashboard_links.endpoint
+  }
+
+  application {
+    name     = juju_application.katib_ui.name
+    endpoint = "dashboard-links"
+  }
+}
+
+resource "juju_integration" "kubeflow_dashboard_katib_ui_links_offer" {
+  count = var.dashboard_links.kind == "offer" ? 1 : 0
+  model_uuid = data.juju_model.kubeflow.uuid
+
+  application {
+    offer_url = var.dashboard_links.url
   }
 
   application {
