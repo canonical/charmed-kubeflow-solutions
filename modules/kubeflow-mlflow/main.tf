3359609dd5ca5453efb8652618f5150efbc85e3b
MattiaSarti <nmattia1@gmail.com>
2026-01-15 13:29:37 +0100

    fix: point to bug-free version of module

--- modules/kubeflow-mlflow/main.tf
+++ modules/kubeflow-mlflow/main.tf
@@ -74,7 +74,7 @@ module "kubeflow" {
 }
 
 module "mlflow" {
-  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=track/2.22"
+  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=4c2d8394b04014d7f3767cf8ebda3a9e751716c7"
   # kubeflow module creates the model
   risk                                  = var.risk
   create_model                          = false
