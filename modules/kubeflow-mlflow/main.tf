e5cfe04ed9871a4ea579f711b0d84c269bb7c957
MattiaSarti <nmattia1@gmail.com>
2026-01-22 10:06:42 +0100

    test: use latest MLflow

--- modules/kubeflow-mlflow/main.tf
+++ modules/kubeflow-mlflow/main.tf
@@ -74,7 +74,7 @@ module "kubeflow" {
 }
 
 module "mlflow" {
-  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=track/2.22"
+  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=kf-8168/test/non-root-charms-and-platform-workloads"
   # kubeflow module creates the model
   risk                                  = var.risk
   create_model                          = false
