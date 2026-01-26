7909d671cfffb418f207493c461b5ed34cdacf80
MattiaSarti <nmattia1@gmail.com>
2026-01-26 10:56:12 +0100

    fix: use correct channel for TrainerV2

--- modules/kubeflow/applications.tf
+++ modules/kubeflow/applications.tf
@@ -257,7 +257,7 @@ module "kubeflow_trainer" {
   source     = "git::https://github.com/canonical/training-operator//terraform?ref=main-v2"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
   revision   = var.kubeflow_trainer_revision
-  channel    = "2.0/edge"
+  channel    = "latest/edge"
 }
 
 module "kubeflow_volumes" {
