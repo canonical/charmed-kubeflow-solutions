235b8e3dc882c95662a0d3d3e293734b5479e2f7
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-02-02 18:03:18 +0545

    [PRA-7] feature: add Kubeflow - Spark integration Terraform bundle (#102)

--- modules/kubeflow/main.tf
+++ modules/kubeflow/main.tf
@@ -1,6 +1,12 @@
 resource "juju_model" "kubeflow" {
   count = var.create_model ? 1 : 0
   name  = local.model
+
+  config = {
+    juju-http-proxy  = var.http_proxy
+    juju-https-proxy = var.https_proxy
+    juju-no-proxy    = var.no_proxy
+  }
 }
 
 locals {
