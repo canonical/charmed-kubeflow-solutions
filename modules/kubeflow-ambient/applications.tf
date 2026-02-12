82cb605b87e29ec4dc0f7d2662b3d8c3ffa58248
NohaIhab <noha.ihab@canonical.com>
2026-02-12 21:44:56 +0200

    set knative channel and branch to 1.16

--- modules/kubeflow-ambient/applications.tf
+++ modules/kubeflow-ambient/applications.tf
@@ -166,18 +166,18 @@ module "knative_eventing" {
     namespace = "knative-eventing"
   }
   revision = var.knative_eventing_revision
-  channel  = "latest/${var.risk}"
+  channel  = "1.16/${var.risk}"
 }
 
 module "knative_operator" {
-  source     = "git::https://github.com/canonical/knative-operators//charms/knative-operator/terraform?ref=main"
+  source     = "git::https://github.com/canonical/knative-operators//charms/knative-operator/terraform?ref=track/1.16"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
   revision   = var.knative_operator_revision
-  channel    = "latest/${var.risk}"
+  channel    = "1.16/${var.risk}"
 }
 
 module "knative_serving" {
-  source     = "git::https://github.com/canonical/knative-operators//charms/knative-serving/terraform?ref=main"
+  source     = "git::https://github.com/canonical/knative-operators//charms/knative-serving/terraform?ref=track/1.16"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
   config = {
     http-proxy                = var.http_proxy,
@@ -188,7 +188,7 @@ module "knative_serving" {
     no-proxy                  = var.no_proxy,
   }
   revision = var.knative_serving_revision
-  channel  = "latest/${var.risk}"
+  channel  = "1.16/${var.risk}"
 }
 
 module "kserve_controller" {
