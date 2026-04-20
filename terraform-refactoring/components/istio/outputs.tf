dc94555e96181f4bbb7c50a88bff5f5e4c48fb34
Dario Faccin <dario.faccin@canonical.com>
2026-04-20 10:16:20 +0200

    add missing outputs

--- terraform-refactoring/components/istio/outputs.tf
+++ terraform-refactoring/components/istio/outputs.tf
@@ -13,6 +13,10 @@ output "provides" {
       name     = juju_application.istio_ingressgateway.name
       endpoint = "gateway"
     }
+    istio_ingressgateway_metrics_endpoint = {
+      name     = juju_application.istio_ingressgateway.name
+      endpoint = "metrics-endpoint"
+    }
     istio_pilot_ingress = {
       name     = juju_application.istio_pilot.name
       endpoint = "ingress"
@@ -20,6 +24,28 @@ output "provides" {
     istio_pilot_ingress_auth = {
       name     = juju_application.istio_pilot.name
       endpoint = "ingress-auth"
+    }
+    istio_pilot_metrics_endpoint = {
+      name     = juju_application.istio_pilot.name
+      endpoint = "metrics-endpoint"
+    }
+    istio_pilot_grafana_dashboard = {
+      name     = juju_application.istio_pilot.name
+      endpoint = "grafana-dashboard"
+    }
+    istio_pilot_gateway_info = {
+      name     = juju_application.istio_pilot.name
+      endpoint = "gateway-info"
+    }
+  }
+}
+
+output "requires" {
+  description = "Map of endpoints required by this component from other components (inbound relations)"
+  value = {
+    istio_pilot_certificates = {
+      name     = juju_application.istio_pilot.name
+      endpoint = "certificates"
     }
   }
 }
