bb1fa193f46068ee7f7afd663e95a80fb1b637df
Dario Faccin <dario.faccin@canonical.com>
2026-04-22 17:01:10 +0200

    do not print juju debug logs (already done in CI step)

--- terraform-refactoring/products/kubeflow/tests/conftest.py
+++ terraform-refactoring/products/kubeflow/tests/conftest.py
@@ -5,20 +5,10 @@ MODEL_NAME = "kubeflow"
 
 @pytest.fixture(scope="module")
 def juju(request: pytest.FixtureRequest):
-    
-    def print_debug_log(juju_instance: jubilant.Juju):
-        if request.session.testsfailed:
-            print(f"[DEBUG] Fetching debug log for model: {juju_instance.model}")
-            log = juju_instance.debug_log(limit=1000)
-            print(log, end="")
-
     juju_instance = jubilant.Juju()
     juju_instance.add_model(MODEL_NAME)
 
-    try:
-        yield juju_instance
-    finally:
-        print_debug_log(juju_instance)
+    yield juju_instance
 
 def pytest_addoption(parser):
     """Add CLI options to pytest."""
