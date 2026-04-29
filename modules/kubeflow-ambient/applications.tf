ccf2173c6aec893cc45764c3981b94d5968f3b59
MattiaSarti <nmattia1@gmail.com>
2026-04-29 10:32:10 +0200

    fix: fix typo over typo fix

--- modules/kubeflow-ambient/applications.tf
+++ modules/kubeflow-ambient/applications.tf
@@ -160,7 +160,7 @@ module "kfp_viz" {
 }
 
 module "kserve_controller" {
-  source     = "git::https://github.com/canonical/kserve-operators//charms/kserve-controller/terraform?ref=track/main"
+  source     = "git::https://github.com/canonical/kserve-operators//charms/kserve-controller/terraform?ref=main"
   model_name = var.create_model ? juju_model.kubeflow[0].name : local.model
   config = {
     deployment-mode = "standard",
