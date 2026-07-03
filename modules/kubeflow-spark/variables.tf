9b1522346125a8e8c679cb2feaf7da1eb8e8cfe5
Amro Elamin <amro.elamin@outlook.com>
2026-07-03 13:34:17 -0400

    fix: remove the hardcoded kubeflow namespace from the public-url vairable in the kubeflow and kubeflow-* modules and support namespace/model flexability (#200). Also added missing attributes in outputs.tf for kubeflow-feast and kubeflow-mlflow modules
    Signed-off-by: Amro Elamin <amro.elamin@outlook.com>

--- modules/kubeflow-spark/variables.tf
+++ modules/kubeflow-spark/variables.tf
@@ -168,6 +168,12 @@ variable "mlmd_size" {
   default     = "10G"
 }
 
+variable "model_name" {
+  description = "Name of the juju model/kubernetes namespace used for the kubeflow deployment"
+  type        = string
+  default     = "kubeflow"
+}
+
 variable "no_proxy" {
   description = "Value of the no_proxy environment variable"
   type        = string
@@ -183,7 +189,7 @@ variable "oidc_gatekeeper_ca_bundle" {
 variable "public_url" {
   description = "Public URL of Kubeflow for auth/OIDC"
   type        = string
-  default     = "http://dex-auth.kubeflow.svc:5556"
+  default     = null
 }
 
 variable "admission_webhook_revision" {
