aee898cec7a0b7cd532c4306d6696e6468f9b9fd
MattiaSarti <nmattia1@gmail.com>
2026-07-23 00:07:01 +0200

    Revert "test: run tests with track 3.14 of MLflow solutions"
    This reverts commit 927a3a27b28a40b872b24514df260371eb418ed7.

--- modules/kubeflow-mlflow/main.tf
+++ modules/kubeflow-mlflow/main.tf
@@ -74,9 +74,9 @@ module "kubeflow" {
 }
 
 module "mlflow" {
-  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=KF-8853/feat/upgrade-mlflow-to-3.14"
+  source = "git::https://github.com/canonical/charmed-mlflow-solutions//modules/mlflow?ref=track/2.22"
   # kubeflow module creates the model
-  risk                                  = "edge"
+  risk                                  = var.risk
   create_model                          = false
   model                                 = module.kubeflow.model
   cos_configuration                     = var.cos_configuration
