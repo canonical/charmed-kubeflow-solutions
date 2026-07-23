170681d0606c8ab12c2d07259e1e77968aa16db0
Enrico Deusebio <enrico.deusebio@canonical.com>
2026-07-24 01:08:17 +0200

    refactor code for improved stability

--- modules/kubeflow-feast/tests/test_deployment.py
+++ modules/kubeflow-feast/tests/test_deployment.py
@@ -1,9 +1,10 @@
 import subprocess
 
-import aiohttp
 import jubilant
 import lightkube
 import pytest
+import requests
+import tenacity
 from itertools import batched
 from lightkube.resources.core_v1 import Service
 
@@ -16,7 +17,7 @@ def lightkube_client() -> lightkube.Client:
 
 class TestCharm:
     @pytest.mark.dependency()
-    async def test_apply_terraform_solution(self, juju: jubilant.Juju, tf_vars):
+    def test_apply_terraform_solution(self, juju: jubilant.Juju, tf_vars):
         """Initialize and apply the kubeflow-feast Terraform solution module."""
         subprocess.run(["terraform", "init"], check=True)
         subprocess.run(
@@ -29,7 +30,7 @@ class TestCharm:
         )
 
     @pytest.mark.dependency(depends=["TestCharm::test_apply_terraform_solution"])
-    async def test_assert_deployment(self, juju: jubilant.Juju, lightkube_client):
+    def test_assert_deployment(self, juju: jubilant.Juju, lightkube_client):
         """
         Wait for the applications to become active and idle and verify its public URL access.
         """
@@ -38,25 +39,31 @@ class TestCharm:
 
         # Remove opentelemetry-collector-k8s from the apps list because it remains
         # `blocked` until it's related to one of the COS charms
-        apps.remove("opentelemetry-collector-k8s-kubeflow")
+        if "opentelemetry-collector-k8s-kubeflow" in apps:
+            apps.remove("opentelemetry-collector-k8s-kubeflow")
 
         for batched_apps in batched(apps, 5):
             juju.wait(lambda status: jubilant.all_active(status, *batched_apps), timeout=3600)
 
         # Verify deployment by checking the public URL
-        url = get_public_url(lightkube_client, "kubeflow")
-        result_status, result_text = await fetch_response(url)
+        url = get_public_url(lightkube_client, "kubeflow", "istio-ingressgateway-workload")
+        result_status, result_text = fetch_response(url)
         assert result_status == 200
         assert "Log in to Your Account" in result_text
         assert "Email Address" in result_text
         assert "Password" in result_text
 
 
-def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
+@tenacity.retry(
+    wait=tenacity.wait_exponential(multiplier=2, min=1, max=10),
+    stop=tenacity.stop_after_attempt(30),
+    reraise=True,
+)
+def get_public_url(
+    lightkube_client: lightkube.Client, bundle_name: str, service_name: str
+):
     """Extracts public URL from service istio-ingressgateway-workload."""
-    ingressgateway_svc = lightkube_client.get(
-        Service, "istio-ingressgateway-workload", namespace=bundle_name
-    )
+    ingressgateway_svc = lightkube_client.get(Service, service_name, namespace=bundle_name)
     address = (
         ingressgateway_svc.status.loadBalancer.ingress[0].hostname
         or ingressgateway_svc.status.loadBalancer.ingress[0].ip
@@ -65,12 +72,12 @@ def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
     return public_url
 
 
-async def fetch_response(url, headers=None):
+@tenacity.retry(
+    wait=tenacity.wait_exponential(multiplier=2, min=1, max=10),
+    stop=tenacity.stop_after_attempt(30),
+    reraise=True,
+)
+def fetch_response(url, headers=None):
     """Fetch provided URL and return (status, text)."""
-    result_status = 0
-    result_text = ""
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=headers) as response:
-            result_status = response.status
-            result_text = await response.text()
-    return result_status, str(result_text)
+    response = requests.get(url, headers=headers)
+    return response.status_code, response.text
