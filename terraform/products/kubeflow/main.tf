8e73fb6ba848928d9b07666e67afd34f3365dccb
mvlassis <manos.vlassis@canonical.com>
2026-07-28 15:28:27 +0300

    fix: Update channel for s3-integrator

--- terraform/products/kubeflow/main.tf
+++ terraform/products/kubeflow/main.tf
@@ -378,7 +378,7 @@ module "s3_global" {
 
   app_name = "s3-integrator-global"
   channel  = local.s3_integrator_channel
-  base = "ubuntu@24.04"
+  base     = "ubuntu@24.04"
   config = merge(
     {
       bucket      = var.s3_bucket_global,
@@ -865,7 +865,7 @@ module "s3_spark" {
 
   app_name = "s3-integrator-spark"
   channel  = local.s3_integrator_channel
-  base = "ubuntu@24.04"
+  base     = "ubuntu@24.04"
   config = merge(
     {
       bucket      = var.s3_bucket_spark,
