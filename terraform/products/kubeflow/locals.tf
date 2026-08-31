7cf3c85bde844a060ec985c1b3aa97c57d3fa3fc
Mattia Sarti <59971270+MattiaSarti@users.noreply.github.com>
2026-08-31 08:29:15 +0200

    chore: upgrade mlflow to 3.15.1 (#262)
    * chore: upgrade mlflow to 3.15.1
    
    * test: reference main branch of UATs

--- terraform/products/kubeflow/locals.tf
+++ terraform/products/kubeflow/locals.tf
@@ -60,7 +60,7 @@ locals {
   resource_dispatcher_channel = var.release == "1.11" ? "2.0/${var.risk}" : "latest/${var.risk}"
 
   # MLflow Component
-  # NOTE: the risk is forced to `edge` for the track `latest` as MLflow 3.14 is available only
+  # NOTE: the risk is forced to `edge` for the track `latest` as MLflow 3.15 is available only
   # on `latest/edge` so far, while changing risk on `latest` would end up deploying MLflow 2.22
   # TODO: parametrize the risk as soon as MLflow 3 is promoted, for the track `latest` here 
   mlflow_channel = var.release == "1.11" ? "2.22/${var.risk}" : "latest/edge"
